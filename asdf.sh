b9c1624a4f3e8b87a65c681b94d474a66fec6fa2
upstreamsea <nathan@notinfosec.com>
2023-10-22 12:55:49 -0500

    asf

--- asdf.sh
+++ asdf.sh
@@ -25,7 +25,7 @@ install_option_brand() {
 
 install_option_staticip() {
     netpackage_config=$(
-        whiptail --title "Network Package" --yesno "Does this location have a ProVision Network Package?" 16 60 3>&2 2>&1 1>&3
+        whiptail --backtitle "VisionPro NVR First Boot Script - October 2023" --title "Network Package" --yesno "Does this location have a ProVision Network Package?" 16 60 3>&2 2>&1 1>&3
     )
     netpackage_config=$?
 }
@@ -34,11 +34,11 @@ install_option_ipmi() {
 
     if [ "$netpackage_config" = "0" ]; then
         ipmi_config=$(
-            whiptail --title "IPMI Configuration" --yesno "Do you wish to configure the IPMI? This configures the Static IP, IPMI hostname, and users." 16 60 3>&2 2>&1 1>&3
+            whiptail --backtitle "VisionPro NVR First Boot Script - October 2023" --title "IPMI Configuration" --yesno "Do you wish to configure the IPMI? This configures the Static IP, IPMI hostname, and users." 16 60 3>&2 2>&1 1>&3
         )
     else
         ipmi_config=$(
-            whiptail --title "IPMI Configuration" --yesno "Do you wish to configure the IPMI? This configures the IPMI hostname and users." 16 60 3>&2 2>&1 1>&3
+            whiptail --backtitle "VisionPro NVR First Boot Script - October 2023"--title "IPMI Configuration" --yesno "Do you wish to configure the IPMI? This configures the IPMI hostname and users." 16 60 3>&2 2>&1 1>&3
         )
     fi
     ipmi_config=$?
@@ -55,22 +55,22 @@ set_hostname() {
         brandhost="nvr1-tbm"
     fi
         
-    hostname=$(whiptail --title "Configure Hostname" --inputbox "Enter desired hostname for this NVR:" 16 100 "$brandhost" 10 30 3>&1 1>&2 2>&3)
+    hostname=$(whiptail --backtitle "VisionPro NVR First Boot Script - October 2023" --title "Configure Hostname" --inputbox "Enter desired hostname for this NVR:" 16 100 "$brandhost" 10 30 3>&1 1>&2 2>&3)
     exitstatus=$?
 
     if [ "$exitstatus" = "1" ]; then
         exit
     else
-        hostnamectl set-hostname ${hostname}
-        echo ${hostname} > /proc/sys/kernel/hostname
-        sed -i 's/127.0.1.1.*/127.0.1.1\t'"${hostname}"'/g' /etc/hosts
+        sudo hostnamectl set-hostname ${hostname}
+        sudo echo ${hostname} > /proc/sys/kernel/hostname
+        sudo sed -i 's/127.0.1.1.*/127.0.1.1\t'"${hostname}"'/g' /etc/hosts
         new_hostname=$(hostname -s)
-        whiptail --ok-button Next --title "Configure Hostname" --msgbox "Hostname set to $new_hostname" 16 60
+        whiptail --ok-button Next --backtitle "VisionPro NVR First Boot Script - October 2023" --title "Configure Hostname" --msgbox "Hostname set to $new_hostname" 16 60
     fi
 }
 
 brand_convert() {
-    whiptail --title "Brand Conversion" --msgbox "Running brand conversion. Please wait..." 16 60
+    whiptail --backtitle "VisionPro NVR First Boot Script - October 2023" --title "Brand Conversion" --msgbox "Running brand conversion. Please wait..." 16 60
 
     if [ "$brand_config" = "AF" ]; then
         busername="club"
@@ -88,43 +88,43 @@ brand_convert() {
  
     if [ "$brand_config" = "WTC" ] || [ "$brand_config" = "BCF" ] || [ "$brand_config" = "TBM" ]; then
         pkill -u club pid
-        # pkill -9 -u club
-        # sleep 10
-        # usermod -l studio club
-        # usermod -d /home/studio -m studio
-        # groupmod -n club studio
-        # chfn -f "studio" studio
+        pkill -9 -u club
+        sleep 10
+        sudo usermod -l studio club
+        sudo usermod -d /home/studio -m studio
+        sudo groupmod -n club studio
+        sudo chfn -f "studio" studio
     fi
 
-    # echo "$busername:$bpassword" | chpasswd
+    echo "$busername:$bpassword" | chpasswd
 
-    whiptail --ok-button "Next" --title "Brand Conversion" --msgbox "Brand conversion complete." 16 60
+    whiptail --backtitle "VisionPro NVR First Boot Script - October 2023" --ok-button "Next" --title "Brand Conversion" --msgbox "Brand conversion complete." 16 60
 }
 
 enable_autologin() {
     output "Enabling auto-login. Please wait..."
 
-    groupadd -r autologin
-    gpasswd -a $busername autologin
-    sed -i "s/pvss/$busername/g" /etc/lightdm/lightdm.conf
+    sudo groupadd -r autologin
+    sudo gpasswd -a $busername autologin
+    sudo sed -i "s/pvss/$busername/g" /etc/lightdm/lightdm.conf
 }
 
 software_install() {
     # Install Required Packages
-    apt install -y libminizip1 cifs-utils net-tools
+    sudo apt install -y libminizip1 cifs-utils net-tools
     output "Installing version 5 of DW Spectrum Server and Client. Please wait..."
     wget https://updates.digital-watchdog.com/digitalwatchdog/37133/linux/dwspectrum-server-5.1.0.37133-linux_x64.deb -O /tmp/dwspectrum-server.deb
     wget https://updates.digital-watchdog.com/digitalwatchdog/37133/linux/dwspectrum-client-5.1.0.37133-linux_x64.deb -O /tmp/dwspectrum-client.deb
-    dpkg -i /tmp/dwspectrum-server.deb
-    apt-get install -f 
-    dpkg -i /tmp/dwspectrum-server.deb
-    apt-get install -f 
+    sudo dpkg -i /tmp/dwspectrum-server.deb
+    sudo apt-get install -f 
+    sudo dpkg -i /tmp/dwspectrum-server.deb
+    sudo apt-get install -f 
     rm -rf /tmp/dwspectrum*
 
     output "Installing TeamViewer Host v15. Please wait..."
     wget https://download.teamviewer.com/download/linux/teamviewer-host_amd64.deb -O /tmp/tv-host.deb
-    dpkg -i /tmp/tv-host.deb
-    apt-get install -f
+    sudo dpkg -i /tmp/tv-host.deb
+    sudo apt-get install -f
     rm -rf /tmp/tv-host.deb
 }
 
@@ -137,31 +137,25 @@ do_ipmi_config() {
     ipmicfg -user add 3 pvss M@sterM1nd123 4
     ipmicfg -user add 4 installer techs1988 2
 
+    ipmicfg -fru BPN "X12STH-F"
+    ipmicfg -fru BP "X12STH-F"
+    ipmicfg -fru PN "VisionPro Network Video Recorder"
+    ipmicfg -fru PV "Generation 4"
+    ipmicfg -fru "ProVision Distribution"
+    ipmicfg -fru CT 05h
+
     # ipmicfg -m 192.168.5.245
     # ipmicfg -k 255.255.255.0
     # ipmicfg -g 192.168.5.1
 
 }
 
-do_harden_tasks() {
-    apt-get update
-    apt-get upgrade -y
-
-    apt purge -y telnet cups modemanager whoopsie zeitgeist-core zeitgeist-datahub
-
-
-}
-
-do_cleanup() {
-
-}
-
 #Execution Steps
-scriptinfo
+#scriptinfo
 install_option_brand
 install_option_staticip
 install_option_ipmi
 set_hostname
-#brand_convert
-#enable_autologin
-#software_install
+brand_convert
+enable_autologin
+software_install
